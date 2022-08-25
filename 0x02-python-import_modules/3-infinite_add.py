902e174283be5ab5815ac2c0224bde4e812ceea5
Daniel <CJNwagbara@hotmail.com>
2022-08-25 18:56:40 +0000

    tsk completed

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
         for temp in range(1, len(sys . argv)):
             num = int(sys . argv[temp])
             sum = sum + num
-            print("{:d}".format(sum))
-        else:
-            print("0")
+        print("{:d}".format(sum))
+    else:
+        print("0")
